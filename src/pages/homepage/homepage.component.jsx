ab985941fc66b5e1e55f1c0cd9bef38443063e46
SproutSeeds <codyshanemitchell@gmail.com>
2021-06-06 20:28:58 -0700

    Broke down homepage into smaller components, added scss to the menu items for styling

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import Direcory from "../../components/directory/directory.component";
+import "./homepage.styles.scss";
+
+const Homepage = () => (
+  <div className="homepage">
+    <Direcory />
+  </div>
+);
+
+export default Homepage;
